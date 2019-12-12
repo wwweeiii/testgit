67b8966556b270d190e0f75c85545287b19ce2a7
IORI <IORI@qq.com>
2019-12-12 17:04:43 +0800

    this is my first commit

---
+++ src/main/java/com/testSpringBoot/Application/SpringCloudGitApplication.java
@@ -0,0 +1,13 @@
+package com.testSpringBoot.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGitApplication.class, args);
+	}
+
+}
